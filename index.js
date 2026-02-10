f28e5d88e21c4c926da36c17bf3a21dc2f16ac45
tomzito1231 <80288965+tomzito1231@users.noreply.github.com>
2026-02-09 21:38:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,33 +27,33 @@ btnNo.addEventListener('click', ()=>{
             case 0:
                 btnNo.innerText = 'Segura?';
                 btnSi.classList.add('estilo-1');
-                randomPic.src = '/assets/2.jpg'
+                randomPic.src = './assets/2.jpg'
                 break;
             
             case 1:
                 btnNo.innerText = 'Segurísima?';
                 btnSi.classList.add('estilo-2');
-                randomPic.src = '/assets/3.jpg'
+                randomPic.src = './assets/3.jpg'
                 break;
         
             case 2:
                 btnNo.innerText = '-_- :( ?';
                 btnSi.classList.add('estilo-3');
-                randomPic.src = '/assets/4.jpg'
+                randomPic.src = './assets/4.jpg'
 
                 break;
 
             case 3:
                 btnNo.innerText = ' OUUU';
                 btnSi.classList.add('estilo-4');
-                randomPic.src = '/assets/5.jpg'
+                randomPic.src = './assets/5.jpg'
 
                 break;
 
             case 4:
                 btnNo.innerText = 'YACHAOMAVARA💔 ';
                 btnSi.classList.add('estilo-5');
-                randomPic.src = '/assets/6.jpg'
+                randomPic.src = './assets/6.jpg'
 
                 break;
 
@@ -62,6 +62,7 @@ btnNo.addEventListener('click', ()=>{
         contador++
         
 })
+
 
 
 
